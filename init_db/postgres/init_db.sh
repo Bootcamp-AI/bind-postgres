a7f198b112676e3f1c2efc833ec098697d1f5c3e
BAISpaces <spaces@bootcampai.org>
2022-04-27 00:31:19 -0500

    Demo 0

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -6,6 +6,6 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Demo PostgreSQL Database initialisation
 psql postgres -c "CREATE USER student PASSWORD 'student'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O testuser testdb
+createdb -O student studentdb
 
-psql -d testdb -U testuser -f $THISDIR/seed_db.sql
+#psql -d studentdb -U student -f $THISDIR/seed_db.sql
